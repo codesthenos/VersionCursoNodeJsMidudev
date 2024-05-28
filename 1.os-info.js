c699c6fc13020f8c8479574cd2f03f9fb8433581
codesthenos <guillermoruizarranz@gmail.com>
2024-05-28 20:30:19 +0200

    Commit 6: Modulo nativo node:os TERMINADO. 1.os-info.js

--- 1.os-info.js
+++ 1.os-info.js
@@ -6,3 +6,4 @@ console.log('Arquitectura del SO', os.arch())
 console.log('CPUs del SO', os.cpus()) // Vamos a poder ESCALAR procesos en Node.js con esto
 console.log('Memoria libre en MEGAS', os.freemem() / 1024 / 1024)
 console.log('Memoria total en MEGAS', os.totalmem() / 1024 / 1024)
+console.log('Dias que lleva encendido el ordenador', os.uptime() / 60 / 60 / 24)
