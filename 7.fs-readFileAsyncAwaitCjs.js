0bd84628b50730b4e033b61099a50ef592f70b7e
codesthenos <guillermoruizarranz@gmail.com>
2024-05-29 20:46:10 +0200

    Commit 13: ASYNC AWAIT en Commonjs con y sin IIFE y en ECMAScript

---
+++ 7.fs-readFileAsyncAwaitCjs.js
@@ -0,0 +1,43 @@
+const fs = require('node:fs/promises')
+
+;(async () => {
+  try {
+    console.log('Leyendo el primer archivo...')
+
+    const text = await fs.readFile('./archivo.txt', 'utf-8')
+
+    console.log('Texto del archivo.txt', text)
+
+    console.log('Hacer cosas mientras lee el archivo...')
+
+    console.log('Leyendo el segundo archivo...')
+
+    const secondText = await fs.readFile('./archivo2.txt', 'utf-8')
+
+    console.log('Texto del archivo2.txt', secondText)
+  } catch (err) {
+      console.error('Error leyendo archivo:', err);
+  }
+})();
+
+async function readFiles() {
+  try {
+    console.log('Leyendo el primer archivo...')
+
+    const text = await fs.readFile('./archivo.txt', 'utf-8')
+
+    console.log('Texto del archivo.txt', text)
+
+    console.log('Hacer cosas mientras lee el archivo...')
+
+    console.log('Leyendo el segundo archivo...')
+
+    const secondText = await fs.readFile('./archivo2.txt', 'utf-8')
+
+    console.log('Texto del archivo2.txt', secondText)
+  } catch (err) {
+    console.error('Error leyendo archivo:', err);
+  }
+}
+
+readFiles()
