fc593a271ecbe572f17ac843b3a05c81d1e137f0
codesthenos <guillermoruizarranz@gmail.com>
2024-05-30 16:02:07 +0200

    Commit 21: Primera version de la funcion final ls

---
+++ 16.-final-ls-AsyncAwaitPromise.js
@@ -0,0 +1,29 @@
+const fs = require('node:fs/promises')
+const path = require('node:path')
+
+const folder = process.argv[2] ?? '.'
+
+async function ls (folder) {
+  let files = []
+  try {
+    files = await fs.readdir(folder)
+    console.log(`El directorio "${folder}" contiene: `)
+  } catch {
+    console.error(`No se pudo leer el directorio ${folder}`)
+    process.exit(1)
+  }
+  
+  const filesPromises = files.map(async file => {
+    const filePath = path.join(folder, file)
+    let stats
+    try {
+      stats = await fs.stat(filePath)
+      console.log(`${file} tiene un tamaño de ${stats.size / 1024}Kb`)
+    } catch {
+      console.error(`No se pudo leer el archivo ${file}`)
+      process.exit(1)
+    }
+  })
+}
+
+ls(folder)
