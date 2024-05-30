7514db52bffd40a07870f08d690c91a3738025e6
codesthenos <guillermoruizarranz@gmail.com>
2024-05-30 16:10:33 +0200

    Commit 22: Mejorada la aplicacion por mi cuenta para que detecte si lo que hay dentro de la carpeta seleccionada es un archivo a una carpeta, si es una carpeta, muestra el nombre y nos dice que es una carpeta, y si es un archivo nos dice tu tamaño en Kb

--- 16.-final-ls-AsyncAwaitPromise.js
+++ 16.-final-ls-AsyncAwaitPromise.js
@@ -16,9 +16,14 @@ async function ls (folder) {
   const filesPromises = files.map(async file => {
     const filePath = path.join(folder, file)
     let stats
+    let isFolder
     try {
       stats = await fs.stat(filePath)
-      console.log(`${file} tiene un tamaño de ${stats.size / 1024}Kb`)
+      if (isFolder = stats.isDirectory()) {
+        console.log(`${file} es una carpeta`)
+      } else if (isFolder = stats.isFile()) {
+        console.log(`El archivo ${file} tiene un tamaño de ${stats.size / 1024}Kb`)
+      }
     } catch {
       console.error(`No se pudo leer el archivo ${file}`)
       process.exit(1)
