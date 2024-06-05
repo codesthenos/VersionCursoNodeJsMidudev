26bb2c14736e8936372e0175faf8d2568bc67ec7
codesthenos <guillermoruizarranz@gmail.com>
2024-06-05 15:27:17 +0200

    Commit 2: ahora el log no muestra el favicon, en el siguiente commit esto estara cambiado

--- Video--2/1.http.js
+++ Video--2/1.http.js
@@ -2,10 +2,14 @@ const http = require('node:http')
 
 const portWanted = process.env.PORT ?? 3000
 
-const server = http.createServer((req, res) => {
-  console.log('request received')
-  res.end('Hola mundo')
-})
+const processRequest = (request, response) => {
+  if (request.url === '/') {
+    console.log('request received: ', request.url)
+  }
+  response.end('Hola mundo')
+}
+
+const server = http.createServer(processRequest)
 
 server.listen(portWanted, () => {
   console.log(`Server listening on http://localhost:${portWanted}`)
