33974600438532325b558970726ce2e1d2d1628e
codesthenos <guillermoruizarranz@gmail.com>
2024-06-05 15:53:17 +0200

    Commit 3: Web con 3 url distintas

--- Video--2/0.webTextoCentrado17Lineas.js
+++ Video--2/0.webTextoCentrado17Lineas.js
@@ -3,16 +3,10 @@ const http = require('node:http')
 const portWanted = process.env.PORT ?? 3000
 
 const processRequest = (request, response) => {
-  response.setHeader('Content-Type', 'text/html; charset=utf-8')
   if (request.url === '/') {
     response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html; charset=utf-8')
     response.end('<h1 style="width:100vw;heigth:100vh;line-height:100vh;text-align:center;">Welcome to myWeb</h1>')
-  } else if (request.url === '/contact') {
-    response.statusCode = 200
-    response.end('<h1>Contact</h1>')
-  } else {
-    response.statusCode = 404
-    response.end('<h1>404</h1>')
   }
 }
 
