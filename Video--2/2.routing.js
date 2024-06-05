30ba543cd4a539c7b93545a1265f0169b7a06696
codesthenos <guillermoruizarranz@gmail.com>
2024-06-05 22:03:31 +0200

    Commit 6: todo como midu, todo funcionando bien, menos el POST, revisar y arreglar y entender porque no funciona antes de seguir con EXPRESS

--- Video--2/2.routing.js
+++ Video--2/2.routing.js
@@ -1,6 +1,8 @@
 const http = require('node:http')
 const tyranitarJSON = require('./POKEMON/tyranitar.json')
 
+const portWanted = process.env.PORT ?? 3000
+
 const processRequest = (req, res) => {
   const { method, url } = req // Con esto saco del objeto request las propiedades method y url y las meto en las const method y url respectivamente
 
@@ -18,8 +20,30 @@ const processRequest = (req, res) => {
 
     case 'POST':
       switch (url) {
-        case '/pokemon':
-          const body = ''
+        case '/pokemon': {
+          let body = ''
+          req.on('data', chunk => {
+            body += chunk.toString()
+          })
+          req.on('end', () => {
+            const data = JSON.parse(body)
+            res.writeHead(201, { 'Content=Type': 'application/json; charset=utf-8' })
+            res.end(JSON.stringify(data))
+          })
+
+          break
+        }
+
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1 style="width:100vw;heigth:100vh;line-height:100vh;text-align:center;">404</h1>')
       }
   }
 }
+
+const server = http.createServer(processRequest)
+
+server.listen(portWanted, () => {
+  console.log(`Server listening on http://localhost:${portWanted}`)
+})
