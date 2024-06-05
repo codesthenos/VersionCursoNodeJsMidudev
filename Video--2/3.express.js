05e0c136c56a78e441366f41a0c32fc4919ce22a
codesthenos <guillermoruizarranz@gmail.com>
2024-06-06 01:00:37 +0200

    Commit 13: usamos middleware para parsear, en el siguiente commit, vamos a reducir el codigo usando una built in function de express express.json() y ademas intentare por mi cuenta para cerrar el curso, hacer que esto funcione en navegador, aunque seguramente lo vere en los siguientes videos de midudev

--- Video--2/3.express.js
+++ Video--2/3.express.js
@@ -6,6 +6,24 @@ const PORT = process.env.PORT ?? 3210
 
 const tyranitarJSON = require('./POKEMON/tyranitar.json')
 
+app.use((req, res, next) => {
+  if (req.method !== 'POST') return next()
+  if (req.headers['content-type'] !== 'application/json; charset=utf-8') return next()
+
+  let body = ''
+
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    data.timestamp = Date.now()
+    req.body = data
+    next()
+  })
+})
+
 app.get('/', (req, res) => {
   res.send('<h1 style="width:100vw;heigth:100vh;line-height:100vh;text-align:center;">Welcome to myWeb</h1>')
 })
@@ -15,16 +33,7 @@ app.get('/pokemon/tyranitar', (req, res) => {
 })
 
 app.post('/pokemon/codesthenos', (req, res) => {
-  let body = ''
-  req.on('data', chunk => {
-    body += chunk.toString()
-  })
-
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    data.timestamp = Date.now()
-    res.status(201).json(data)
-  })
+  res.status(201).json(req.body)
 })
 
 app.use((req, res) => {
