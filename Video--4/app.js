fbf79cd9dd039b4c364c00c785daa5466e690484
codesthenos <guillermoruizarranz@gmail.com>
2024-06-09 12:51:51 +0200

    Commit 2: Primer paso para ordenar las cosas, nos sacamos la lectura del json a utils.js y ademas en vez de crear la constante del json leido directamente, creamos una funcion que puede leer json en general y la usamos en app.js para traernos el json

--- Video--4/app.js
+++ Video--4/app.js
@@ -4,20 +4,9 @@ import cors from 'cors'
 
 import { validateNewMovie, validatePartialMovie } from './MovieStructure/movies.js'
 
-// import moviesJSON from './movies.json' with FORMA DE IMPORTAR .json EN FUTURO PROXIMO
+import { readJSON } from './utils.js'
 
-/* FORMA ACTUAL 1 IMPORTANDO fs Y METIENDO EN UNA CONSTANTE EL RESULTADO DEL PARSEO Y LECTURA DEL JSON
-  import fs from 'node:fs'
-  const moviesJSON = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
-*/
-
-// CREANDO LA FUNCION 'require()' DE CommonJS Y UNA VEZ CREADA, IMPORTAR EL '.json' DIRECTAMENTE COMO HACIAMOS EN CommonJS
-// IMPORTAMOS 'createRequire()' PARA PODER CREAR LA FUNCION require EN ESModules
-import { createRequire } from 'node:module'
-// CREAMOS LA FUNCION 'require()' USANDO 'import.meta.url' QUE NOS DEVUELVE LA DIRECCION DEL ARCHIVO EN EL QUE ESTAMOS
-const require = createRequire(import.meta.url)
-// NOS TRAEMOS EL '.json' COMO HACIAMOS CUANDO USABAMOS CommonJS
-const moviesJSON = require('./movies.json')
+const moviesJSON = readJSON('./movies.json')
 
 const app = express()
 
