0ff3aa2c250106f49f92f1a523aa854ff3c2a21e
codesthenos <guillermoruizarranz@gmail.com>
2024-07-04 21:43:31 +0200

    Commit 4: instalo mysql2 y dejo preparado el movie.js de mysql para comenzar la conexion

---
+++ Video--5/models/mysql/movie.js
@@ -0,0 +1,25 @@
+export class MovieModel {
+  static async getAll ({ director, rate }) {
+
+  }
+
+  static async getById ({ id }) {
+
+  }
+
+  static async getByGenre ({ genre }) {
+
+  }
+
+  static async create ({ input }) {
+
+  }
+
+  static async delete ({ id }) {
+
+  }
+
+  static async update ({ id, input }) {
+
+  }
+}
