3a403199655aa3ba82d3a5f679adc3c8d24f10ea
codesthenos <guillermoruizarranz@gmail.com>
2024-07-05 18:07:07 +0200

    Commit 7: todos los metodos GET funcionando como espero, quedan el post (create), patch (update) y delete

--- Video--5/models/mysql/movie.js
+++ Video--5/models/mysql/movie.js
@@ -12,7 +12,7 @@ const connection = await mysql.createConnection(config)
 
 export class MovieModel {
   static async getAll ({ director, rate }) {
-    let query = 'SELECT * FROM movie'
+    let query = 'SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate FROM movie'
     const queryParams = []
 
     if (director || (rate && rate > 0 && rate <= 10)) {
@@ -30,17 +30,47 @@ export class MovieModel {
       }
     }
 
-    const [result] = await connection.query(query, queryParams)
+    let [result] = await connection.query(query, queryParams)
+
+    if (result.length > 0) {
+      return result
+    }
+
+    [result] = await connection.query('SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate FROM movie')
 
     return result
   }
 
   static async getById ({ id }) {
+    let [result] = await connection.query(
+      `SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate
+        FROM movie WHERE id = UUID_TO_BIN(?)`, [id]
+    )
 
+    if (result.length > 0) {
+      return result
+    }
+
+    [result] = await connection.query('SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate FROM movie')
+
+    return result
   }
 
   static async getByGenre ({ genre }) {
+    let [result] = await connection.query(
+      `SELECT BIN_TO_UUID(movie.id) id, movie.title, movie.year, movie.director, movie.duration, movie.poster, movie.rate FROM movie 
+       JOIN movie_genre ON movie.id = movie_genre.movie_id 
+       JOIN genre ON movie_genre.genre_id = genre.id
+       WHERE genre.name = ?`, [genre]
+    )
 
+    if (result.length > 0) {
+      return result
+    }
+
+    [result] = await connection.query('SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate FROM movie')
+
+    return result
   }
 
   static async create ({ input }) {
