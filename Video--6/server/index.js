c644846af99dd7021d496a97c0291944a2ff8a5b
codesthenos <guillermoruizarranz@gmail.com>
2024-07-08 21:02:39 +0200

    Commit 3: agrego middleware para poder linkear archivos estaticos como el styles.css al index.html, y hago que el index.js cargue el index.html en vez de un string

--- Video--6/server/index.js
+++ Video--6/server/index.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import logger from 'morgan'
+import path from 'path'
 
 const PORT = process.env.PORT ?? 3210
 const app = express()
 
 app.use(logger('dev'))
 
+app.use(express.static(path.join(process.cwd(), 'client')))
+
 app.get('/', (req, res) => {
-  res.send('<h1>Live Chat</h1>')
+  res.sendFile(path.join(process.cwd() + 'client/index.html'))
 })
 
 app.listen(PORT, () => {
