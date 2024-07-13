77491626a547ec750d7f976209150fcf7db3cebe
codesthenos <codesthenos@gmail.com>
2024-07-13 16:57:48 +0200

    Commit 7: incluyo el boton send en el html y en el script de js ahora nos traemos del documento tanto el input como el formulario, y solo capaces de enviar mensajes al servidor, aunque el server aun no hace nada con ellos

--- Video--6/client/script.js
+++ Video--6/client/script.js
@@ -2,3 +2,15 @@ import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js'
 
 // eslint-disable-next-line no-unused-vars
 const socket = io()
+
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  if (input.value) {
+    socket.emit('chat message', input.value)
+    input.value = ''
+  }
+})
