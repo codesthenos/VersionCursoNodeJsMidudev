e311b8112867507269f9c59bc1f0cb444b84098e
codesthenos <codesthenos@gmail.com>
2024-07-14 12:46:28 +0200

    Commit 12: Web funcionando como la de midudev, cada browser crea un usuario nuevo y lo recuerda entre sesiones, y en el mensaje va el nombre del usuario, en el siguiente commit, creare una copia que cada vez que se termina sesion se adquieren unos datos random de usuario, es decir que no se guardan, nunca tendremos el mismo usuario

--- Video--6/client/script.js
+++ Video--6/client/script.js
@@ -1,8 +1,24 @@
 import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js'
 
-// eslint-disable-next-line no-unused-vars
+const getUserName = async () => {
+  // eslint-disable-next-line no-undef
+  const username = localStorage.getItem('username')
+  if (username) {
+    console.log(`Username exists ${username}`)
+    return username
+  }
+
+  const res = await fetch('https://random-data-api.com/api/users/random_user')
+  const { username: randomUsername } = await res.json()
+
+  // eslint-disable-next-line no-undef
+  localStorage.setItem('username', randomUsername)
+  return randomUsername
+}
+
 const socket = io({
   auth: {
+    username: await getUserName(),
     serverOffset: 0
   }
 })
@@ -11,8 +27,10 @@ const form = document.getElementById('form')
 const input = document.getElementById('input')
 const messages = document.getElementById('messages')
 
-socket.on('chat message', (message, serverOffset) => {
-  const item = `<li>${message}</li>`
+socket.on('chat message', (message, serverOffset, username) => {
+  const item = `<li>
+                <p><small>${username}: </small>${message}</p>
+                </li>`
   messages.insertAdjacentHTML('beforeend', item)
   socket.auth.serverOffset = serverOffset
 })
