93a4ed9c2b48baa13c626c1aaa257c317c97a155
codesthenos <codesthenos@gmail.com>
2024-07-14 13:04:42 +0200

    Commit 13: Creo 3 scripts para la misma app, cada uno con un comportamiento de guardado de usuario distinto, uno que deja un usuario permanente en cada browser (localStorage), otro que lo deja permanente en cada pestaña, pero si abro una nueva pesataña tengo otro usuario (sessionStorage) y otro que solo me guarda el usuario siempre y cuando no se refresque o se pierda conexion (noStorage)

---
+++ Video--6/client/noStorageScript.js
@@ -0,0 +1,36 @@
+import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js'
+
+const getUserName = async () => {
+  const res = await fetch('https://random-data-api.com/api/users/random_user')
+  const { username: randomUsername } = await res.json()
+
+  return randomUsername
+}
+
+const socket = io({
+  auth: {
+    username: await getUserName(),
+    serverOffset: 0
+  }
+})
+
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const messages = document.getElementById('messages')
+
+socket.on('chat message', (message, serverOffset, username) => {
+  const item = `<li>
+                <p><small>${username}: </small>${message}</p>
+                </li>`
+  messages.insertAdjacentHTML('beforeend', item)
+  socket.auth.serverOffset = serverOffset
+})
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  if (input.value) {
+    socket.emit('chat message', input.value)
+    input.value = ''
+  }
+})
